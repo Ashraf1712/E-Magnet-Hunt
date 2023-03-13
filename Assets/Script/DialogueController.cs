2a659fe4cbda9b96154cf4409d3e5de4e30e8037
Ashraf1712 <ashrafaiman1712@gmail.com>
2023-03-14 00:17:39 +0800

    Updated Feedback with 2 Link

--- Assets/Script/DialogueController.cs
+++ Assets/Script/DialogueController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.Profiling.Editor;
 using UnityEngine;
 
 public class DialogueController : MonoBehaviour
